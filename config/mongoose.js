01a0e366f68227efe98954531ba78ff482ca67d5
Smit Patel <patelsmit@Smits-MacBook-Air.local>
2021-06-16 21:06:11 +0530

    ddfdvdhhjhjhh

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,17 +3,21 @@ const mongoose =require('mongoose');
 const dotenv = require('dotenv');
 const db = process.env.MONGODB_URI;
 
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
+// mongoose.connect(process.env.MONGODB_URI,{
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     useUnifiedTopology:true,
+//     useFindAndModify:false
 
-}).then(()=>{
-    console.log(`connected sucessfully`);
-}).catch((err)=>console.log(`no connection bhaiiii`));
-
-
+// }).then(()=>{
+//     console.log(`connected sucessfully`);
+// }).catch((err)=>console.log(`no connection bhaiiii`));
+try{
+mongoose.connect( MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+console.log("connected"));    
+}catch (error) { 
+console.log("could not connect");  
+}
 
 // db.on('error',console.error.bind(console,"Error n conection"))
 
