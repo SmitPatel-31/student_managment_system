fa031b237d69662991f2954e972c21626e6dc966
Smit Patel <patelsmit@Smits-MacBook-Air.local>
2021-06-16 19:33:18 +0530

    ddfd

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 
-
+const dotenv = require('dotenv');
 const db = process.env.MONGODB_URI||'mongodb+srv://smit:smitp31076351@cluster0.rzg9x.mongodb.net/students?retryWrites=true&w=majority';
 
 mongoose.connect(process.env.MONGODB_URI || db,{
